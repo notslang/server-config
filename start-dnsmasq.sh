c717b22767728a6b776b85fd47c1672b805a386f
Sean Lang <slang800@gmail.com>
2021-03-27 16:18:45 -0500

    remove old / unused stuff

--- start-dnsmasq.sh
+++
@@ -1,5 +0,0 @@
-docker run --name dnsmasq -d --rm \
-        --net=host \
-        --cap-add=NET_ADMIN \
-        -v $(pwd)/matchbox-dnsmasq.conf:/etc/dnsmasq.conf:Z \
-        quay.io/coreos/dnsmasq:v0.5.0
--- start-matchbox.sh
+++
@@ -1,5 +0,0 @@
-docker run --name matchbox -d --rm \
-        -p 8080:8080 \
-        -v $PWD/examples:/var/lib/matchbox:Z \
-        -v $PWD/examples/groups/etcd3:/var/lib/matchbox/groups:Z \
-        quay.io/coreos/matchbox:latest -address=0.0.0.0:8080 -log-level=debug
