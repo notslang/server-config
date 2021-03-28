a4d0349da4a946c5ff5cb6e3218b546ae83f989b
Sean Lang <slang800@gmail.com>
2021-03-27 20:24:29 -0500

    add script to start nginx

---
+++ script/start-nginx.sh
@@ -0,0 +1,11 @@
+docker run -d --name nginx \
+       -v /data/nginx/log:/var/log/nginx/ \
+       -v /data/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+       -v /data/nginx/config:/etc/nginx/conf.d:ro \
+       -v /data/nginx/content:/usr/share/nginx/html:ro \
+       -v /data/certbot:/etc/letsencrypt:ro \
+       -p 80:80 \
+       -p 443:443 \
+       -p 6984:6984 \
+       -p 5984:5984 \
+       nginx:1.19.8-alpine
